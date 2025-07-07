1b573b891770d4edcdcbde339661631b33f8277f
Sehtaj Singh <batthsehtaj10@gmail.com>
2025-07-08 00:19:52 +0530

    Updated twoSumSorted.py

--- python/twoPointers/twoSumSorted.py
+++ python/twoPointers/twoSumSorted.py
@@ -1,3 +1,8 @@
+# twoSum: List[int], int -> List[int]
+
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
 def twoSum(nums, target):
     l = 0
     r = len(nums) - 1
@@ -11,9 +16,10 @@ def twoSum(nums, target):
         else: 
             r -= 1
     
-print(twoSum([1,2,3,4], 7))
-print(twoSum([2,7,11,15], 9))
-print(twoSum([2,3,4], 6))
-print(twoSum([-1,0], -1))
+# edge cases
+# print(twoSum([1, 2, 3, 4], 7))      => [3, 4]     (3 + 4 = 7)
+# print(twoSum([2, 7, 11, 15], 9))    => [1, 2]     (2 + 7 = 9)
+# print(twoSum([2, 3, 4], 6))         => [1, 3]     (2 + 4 = 6)
+# print(twoSum([-1, 0], -1))          => [1, 2]     (-1 + 0 = -1)
 
     
