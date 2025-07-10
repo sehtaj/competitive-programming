c21e2122ce13483c0723449e0df764be8a5abd0c
Sehtaj Singh <batthsehtaj10@gmail.com>
2025-07-10 23:36:36 +0530

    Updated dailyTemperatures.py

--- python/stack/dailyTemperatures.py
+++ python/stack/dailyTemperatures.py
@@ -1,3 +1,8 @@
+# dailyTemperature : List[int] -> List[int]
+
+# Time Complexity: O(n) 
+# Space Complexity: O(n)
+
 def dailyTemperature(temperatures):
     answer = [0] * (len(temperatures))
     stack = []
@@ -12,4 +17,23 @@ def dailyTemperature(temperatures):
     return answer
             
 
-print(dailyTemperature([73,74,75,71,69,72,76,73]))
+# dailyTemperature([73, 74, 75, 71, 69, 72, 76, 73])
+# Output: [1, 1, 4, 2, 1, 1, 0, 0]
+
+# dailyTemperature([30, 40, 50, 60])
+# Output: [1, 1, 1, 0]
+
+# dailyTemperature([60, 50, 40, 30])
+# Output: [0, 0, 0, 0]
+
+# dailyTemperature([50, 50, 50, 50])
+# Output: [0, 0, 0, 0]
+
+# dailyTemperature([30, 31, 30, 29, 35])
+# Output: [1, 3, 2, 1, 0]
+
+# dailyTemperature([70])
+# Output: [0]
+
+# dailyTemperature([])
+# Output: []
